e3e77ff13a5fa2d18f0802a5daeaa872f4b675e7
Hao Chen <areigna@gmail.com>
2014-12-23 19:38:46 +0000

    updates

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 //app.engine('jade', require('jade').__express);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+
 app.use('/static',express.static(__dirname + '/public'));
 
 //fetch images
